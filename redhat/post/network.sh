5dd4e7d0cae51f47bd232d118b924bf250250578
Daniel Rahamim <drahamim@seven.com>
2013-03-18 13:54:29 -0700

    updating network.sh

--- redhat/post/network.sh
+++ redhat/post/network.sh
@@ -7,77 +7,18 @@
 case "$1" in 
 	start)
 
-echo "Fully Qualified Domain Name of this Host?"
-read FQDN
+hostmac=`cat /etc/sysconfig/network-scripts/ifcfg-eth0|grep "HWADDR" |cut -d"\"" -f2`
 
-echo "What is to be the IP?"
-read address
+infoline=`cat /tmp/network.tab | grep -i $hostmac`
 
-net=`echo $address|cut -d'.' -f3`
-echo $net
-
-if[ 1 <= $net <= 5]
-then
-	network=10.1.0.0
-	netmask=255.255.248.0
-	gateway=10.1.0.1
-
-elif[16 <= $net <= 23]
-then 
-        network=10.1.16.0
-        netmask=255.255.248.0
-        gateway=10.1.16.1
-
-elif[24 <= $net <= 31]
-then 
-        network=10.1.24.0
-        netmask=255.255.248.0
-        gateway=10.1.24.1
-elif[32 <= $net <= 39]
-then
-        network=10.1.32.0
-        netmask=255.255.248.0
-        gateway=10.1.32.1
-elif[40 <= $net <= 47]
-then
-        network=10.1.40.0
-        netmask=255.255.248.0
-        gateway=10.1.40.1
-elif[48 <= $net <= 55]
-then
-        network=10.1.48.0
-        netmask=255.255.248.0
-        gateway=10.1.48.1
-elif[56 <= $net <= 63]
-then
-        network=10.1.56.0
-        netmask=255.255.248.0
-        gateway=10.1.56.1
-elif[64 <= $net <= 71]
-then
-        network=10.1.64.0
-        netmask=255.255.248.0
-        gateway=10.1.64.1
-elif[72 <= $net <= 79]
-then
-        network=10.1.72.0
-        netmask=255.255.248.0
-        gateway=10.1.72.1
-elif[$net = 130]
-then
-        network=10.1.130.0
-        netmask=255.255.255.0
-        gateway=10.1.130.1
-else	
-	echo "incorrect IP"
-	exit
-
-fi
+FQDN=`echo $infoline |cut -d',' -f2`
+address=`echo $infoline |cut -d',' -f3`
+netmask=`echo $infoline |cut -d',' -f4`
+gateway=`echo $infoline |cut -d',' -f5`
 
 
-echo "Configuring Hostname"
 
-hostname $FQDN
+sed -i.BAK -e "s/HOSTNAME=.*/HOSTNAME=$FQDN/" /etc/sysconfig/network
 
 echo "Configuring Bond0 Master Interface"
 cat << EOF >> /tmp/ifcfg-bond0.temp
@@ -120,7 +61,6 @@ EOF
 
 macaddr=`cat /etc/sysconfig/network-scripts/ifcfg-eth1|grep HWADDR`
 
-echo "$macaddr"
 echo "$macaddr" >> /tmp/ifcfg-eth1.temp
 
 mv /tmp/ifcfg-bond0.temp /etc/sysconfig/network-scripts/ifcfg-bond0
